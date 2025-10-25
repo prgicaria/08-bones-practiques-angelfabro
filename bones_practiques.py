2a484de2213042c790ff43dccb15988bfba1802a
ángel <afabro@instituticaria.cat>
2025-10-25 18:19:28 +0200

    e

--- bones_practiques.py
+++ bones_practiques.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-'''bones_practiques.py. Escriure un programa que faci 
+'''bones_practiques.py. Escriure un programa que faci
 una divisió
 
 Institut Icària
 Programació - 1r Batxillerat - Curs 2024-25
-Programa que demana a l'usuari dos nombres 
+Programa que demana a l'usuari dos nombres
 enters: dividend i divisor.
-La sortida per pantalla ha de mostrar la 
+La sortida per pantalla ha de mostrar la
 divisió, el quocient i el residu.
 '''
 __author__ = "Ángel Fabro Francia"
@@ -18,5 +18,5 @@ divisor = int(input("Introdueix el divisor: "))
 quocient = dividend // divisor
 residu = dividend % divisor
 print(f"Divisió: {dividend}/{divisor}")
-print("Quocient:", quocient)
-print("Residu: ", residu)
+print(f"Quocient: {quocient}")
+print(f"Residu:  {residu}")
